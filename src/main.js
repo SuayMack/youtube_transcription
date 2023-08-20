fd890bc945946ff3cdc9591e67c8383ad4ef73cf
SuayMack <pry_chu@hotmail.com>
2023-08-20 01:22:08 -0300

    feat: created html and css structure

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import '../styles/global.css';
+import '../styles/base.css';
+import '../styles/form.css';
+import '../styles/transcription.css';
+import '../styles/loading.css';
+import '../styles/copy.css';
